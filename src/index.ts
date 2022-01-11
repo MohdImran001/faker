09c937a1531ec2e46827f23c69371fe3df978645
Shinigami92 <chrissi92@hotmail.de>
2022-01-11 13:54:47 +0100

    feat: migrate helpers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Datatype } from './datatype';
+import { Helpers } from './helpers';
 import { Mersenne } from './mersenne';
 import { Random } from './random';
 
@@ -153,6 +154,7 @@ export class Faker {
 
   readonly mersenne: Mersenne = new Mersenne();
   random: Random = new Random(this);
+  readonly helpers: Helpers = new Helpers(this);
   datatype: Datatype = new Datatype(this);
 
   constructor(opts: FakerOptions = {}) {
